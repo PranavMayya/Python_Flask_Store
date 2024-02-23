585872a24ab03cee6ef985cc6713bd237f224412
PranavMayya <134544463+PranavMayya@users.noreply.github.com>
2024-02-23 17:55:57 +0530

    Update __init__.py

--- market/__init__.py
+++ market/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 app = Flask(__name__)
 #for more details about Flask-SqlAlchemy visit- https://www.tutorialspoint.com/flask/flask_sqlalchemy.htm
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '6a37afee4c190d461bd1dcd6'
+app.config['SECRET_KEY'] = '9a37afee4c160d491db1dcb9'
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
 login_manager = LoginManager(app)
